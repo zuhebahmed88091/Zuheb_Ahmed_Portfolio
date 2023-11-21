dfe57e0cbb9e96223a97469cc6e1b3dca64c6d75
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-11-21 11:07:19 +0600

    Test link styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./app.scss";
 
 const App = () => {
   return <div>
-    <section>Intro</section>
+    <section><a href="">Hello</a></section>
     <section>Parallax</section>
     <section>Skills</section>
     <section>Parallax</section>
