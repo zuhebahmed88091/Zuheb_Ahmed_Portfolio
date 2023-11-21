08eb13073ba55761834e1ee27e984429535cf1a9
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-11-21 12:33:45 +0600

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import "./app.scss";
+import { Navbar } from "./components/navbar/Navbar";
 
 const App = () => {
   return <div>
-    <section><a href="">Hello</a></section>
+    <section>
+      <Navbar/>
+    </section>
     <section>Parallax</section>
     <section>Skills</section>
     <section>Parallax</section>
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import "./navbar.scss"
+import { motion } from "framer-motion"
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div className="wrapper">
+            <motion.span
+             initial={{opacity: 0, scale: 0.5}}
+             animate={{opacity: 1, scale: 1}}
+             transition={{duration: 0.5}}
+            >
+                Zuheb Ahmed
+            </motion.span>
+            <div className="social">
+                <a href="https://www.linkedin.com/in/zuheb-ahmed/">
+                    <img src="/linkedin.png" alt="logo" />
+                </a>
+                <a href="https://twitter.com/ZuhebAhmed88091">
+                    <img src="/twitter.png" alt="logo" />
+                </a>
+                <a href="https://medium.com/@zuhebahmed101625">
+                    <img src="/medium.png" alt="logo" />
+                </a>
+                <a href="https://github.com/zuhebahmed88091">
+                    <img src="/github.png" alt="logo" />
+                </a>
+                <a href="https://wellfound.com/u/zuheb-ahmed-1">
+                    <img src="/angelist.png" alt="logo" />
+                </a>
+            </div>
+        </div>
+    </div>
+  )
+}
