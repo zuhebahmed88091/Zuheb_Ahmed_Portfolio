6844e31ab0ed4168d3561acebe35b4093f06c6f3
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-11-22 16:29:06 +0600

    Add skills part

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./app.scss";
 import { Intro } from "./components/intro/Intro";
 import { Navbar } from "./components/navbar/Navbar";
 import { Parallax } from "./components/parallax/Parallax";
+import { Skills } from "./components/skills/Skills";
 
 const App = () => {
   return <div>
@@ -10,11 +11,12 @@ const App = () => {
       <Intro/>
     </section>
     <section id="Skills"><Parallax type="skills"/></section>
-    <section>Skills</section>
+    <section><Skills/></section>
     <section id="Projects"><Parallax type="projects"/></section>
     <section>Project1</section>
     <section>Project1</section>
     <section>Project1</section>
+    <section id="About Me">About Me</section>
     <section id="Contact">Contact</section>
   </div>;
 };
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,81 @@
+import "./skills.scss";
+import { motion } from "framer-motion"
+
+const variants = {
+    initial: {
+        x: -500,
+        y: 100,
+        opacity: 0
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1
+        }
+    }
+}
+
+export const Skills = () => {
+  return (
+    <motion.div className="skills" variants={variants} initial="initial" whileInView="animate">
+      <motion.div className="titleContainer" variants={variants}>
+        <div className="title">
+            <img src="/people.webp" alt="people" />
+            <h1>Crafting<motion.b whileHover={{color:"orange"}}> Solutions</motion.b></h1>
+        </div>
+        <div className="title">
+            <h1>With Diverse <motion.b whileHover={{color:"orange"}}>Skills</motion.b></h1>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer" variants={variants}>
+        <div className="box">
+            <h2>Front-End</h2>
+            <ul>
+                <li>CSS3</li>
+                <li>React</li>
+                <li>Redux</li>
+                <li>HTML5</li>
+                <li>Bootstrap</li>
+                <li>JavaScript</li>
+                <li>Single Page App</li>
+                <li>Responsive Design</li>
+            </ul>
+        </div>
+        <div className="box">
+            <h2>Back-End</h2>
+            <ul>
+                <li>Ruby</li>
+                <li>PostgreSQL</li>
+                <li>Ruby on Rails</li>
+            </ul>
+        </div>
+        <div className="box">
+            <h2>Tools & Methods</h2>
+            <ul>
+                <li>Git</li>
+                <li>TDD</li>
+                <li>Jest</li>
+                <li>Rspec</li>
+                <li>Github</li>
+                <li>Render</li>
+                <li>Webpack</li>
+            </ul>
+        </div>
+        <div className="box">
+            <h2>Professional</h2>
+            <ul>
+                <li>Leadership</li>
+                <li>Remote Work</li>
+                <li>Problem Solving</li>
+                <li>Pair-Programming</li>
+                <li>Team Collaboration</li>
+                <li>Conflict Management</li>
+            </ul>
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+};
